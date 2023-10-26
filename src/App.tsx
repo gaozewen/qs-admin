52eb85f065a7e3bd808d95db298c857f1902f311
gaozewen <1440651163@qq.com>
2023-10-27 00:44:31 +0800

    style: 问卷列表页简单布局，问卷卡片组件简单布局

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>问卷系统</h1>
       <List />
     </div>
--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,16 +1,17 @@
 import React, { FC, useState } from 'react'
 // import QuestionCard from '../components/QuestionCard'
-// import styles from './List.module.scss'
+import styles from './List.module.scss'
+import QuestionCard from '../components/QuestionCard'
 
 const mockListData = Array(10)
   .fill('mock')
   .map((item, index) => ({
-    id: `q${index}`,
+    _id: `q${index}`,
     title: `问卷${index}`,
-    isPublished: false,
+    isPublished: [true, false][index % 2],
     isStar: false,
     answerCount: index % 5,
-    createAt: `10月2${index}日 1${index}:4${index}`,
+    createdAt: `10月2${index}日 1${index}:4${index}`,
   }))
 
 const List: FC = () => {
@@ -18,10 +19,18 @@ const List: FC = () => {
 
   return (
     <>
-      <p>List</p>
-      {list.map(item => (
-        <div key={item.id}>{item.title}</div>
-      ))}
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <h3>我的问卷</h3>
+        </div>
+        <div className={styles.right}>搜索</div>
+      </div>
+      <div className={styles.content}>
+        {list.map(item => (
+          <QuestionCard key={item._id} {...item} />
+        ))}
+      </div>
+      <div className={styles.footer}>footer</div>
     </>
   )
 }

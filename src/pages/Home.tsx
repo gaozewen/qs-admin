990396586157234c44f17bc1ef239c05e93dd5cd
gaozewen <1440651163@qq.com>
2023-10-28 07:45:23 +0800

    style: 编写 Home 页，并使用渐变色网站的渐变色 CSS

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,25 @@
 import { Button } from 'antd'
 import React, { FC } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { Typography } from 'antd'
+import { PN_MANAGE_LIST } from '../router'
+import styles from './Home.module.scss'
+
+const { Title, Paragraph } = Typography
 
 const Home: FC = () => {
   const nav = useNavigate()
-  const onNavToLogin = () => {
-    // nav('/login')
-    nav({
-      pathname: '/login',
-      search: 'b=21',
-    })
-  }
 
   return (
-    <div>
-      <p>Home</p>
-      <div>
-        <Button onClick={onNavToLogin}>登录</Button>
-        <Link to="/register?a=10">注册</Link>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <Title>问卷调查 | 在线投票</Title>
+        <Paragraph>已累计创建问卷 188 份，发放问卷 88 份，收到答卷 988 份</Paragraph>
+        <div>
+          <Button type="primary" onClick={() => nav(PN_MANAGE_LIST)}>
+            开始使用
+          </Button>
+        </div>
       </div>
     </div>
   )

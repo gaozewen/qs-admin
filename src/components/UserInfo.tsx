80e85947db03b489ebd61402e81efab4ee844059
gaozewen <1440651163@qq.com>
2023-10-31 17:45:27 +0800

    feat: 完善 UserInfo 组件，实现用户信息展示和登出功能

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,13 +1,40 @@
 import React, { FC } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useRequest } from 'ahooks'
+import { UserOutlined } from '@ant-design/icons'
+import { Button, Space } from 'antd'
 import { PN_LOGIN } from '../router'
+import { getUserInfoService } from '../services/user'
+import { removeToken } from '../utils/user-token'
 
 const UserInfo: FC = () => {
-  return (
+  const { data } = useRequest(getUserInfoService)
+  const { username, nickname } = data || {}
+  const nav = useNavigate()
+
+  const onLogout = () => {
+    // 清除 token 的存储
+    removeToken()
+    nav(PN_LOGIN)
+  }
+
+  const UserInfo = (
     <>
-      <Link to={PN_LOGIN}>登录</Link>
+      <span style={{ color: '#e8e8e8' }}>
+        <Space>
+          <UserOutlined />
+          {nickname}
+        </Space>
+      </span>
+      <Button type="link" onClick={onLogout}>
+        退出
+      </Button>
     </>
   )
+
+  const Login = <Link to={PN_LOGIN}>登录</Link>
+
+  return username ? UserInfo : Login
 }
 
 export default UserInfo

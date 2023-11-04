b4ad09338c3e6b276293464480b2131460c641fd
GaoZeWen <1440651163@qq.com>
2023-11-04 16:07:26 +0800

    feat: 实现自动保存（监听要保存数据的变化，使用防抖 1s 保存一次增加用户体验同时减轻服务器压力）

--- src/pages/questionnaire/edit/HeaderPanel/SaveButton/index.tsx
+++ src/pages/questionnaire/edit/HeaderPanel/SaveButton/index.tsx
@@ -1,4 +1,4 @@
-import { useKeyPress, useRequest } from 'ahooks'
+import { useDebounceEffect, useKeyPress, useRequest } from 'ahooks'
 import { Button, message } from 'antd'
 import React, { FC } from 'react'
 import { useParams } from 'react-router-dom'
@@ -8,23 +8,31 @@ import useGetQEditorInfo from '../../../../../hooks/useGetQEditorInfo'
 const SaveButton: FC = () => {
   const { id = '' } = useParams()
   const { componentList, pageInfo } = useGetQEditorInfo()
-  const { loading, run: onSave } = useRequest(
-    async () => {
-      if (!id) return
-      await updateQuestionnaireService(id, { ...pageInfo, componentList })
+  const onSaveHandler = async () => {
+    if (!id) return
+    await updateQuestionnaireService(id, { ...pageInfo, componentList })
+  }
+  const { loading, run: onSave } = useRequest(onSaveHandler, {
+    manual: true,
+    onSuccess() {
+      message.success('保存成功')
     },
-    {
-      manual: true,
-      onSuccess() {
-        message.success('保存成功')
-      },
-    }
-  )
+  })
 
   useKeyPress(['ctrl.s', 'meta.s'], (event: KeyboardEvent) => {
     event.preventDefault()
     if (!loading) onSave()
   })
+
+  // 自动保存（监听要保存数据的变化，使用防抖 1s 保存一次增加用户体验同时减轻服务器压力）
+  useDebounceEffect(
+    () => {
+      // 直接使用，因为不需要 loading 和 toast
+      onSaveHandler()
+    },
+    [pageInfo, componentList],
+    { wait: 1000 }
+  )
 
   return (
     <Button loading={loading} onClick={onSave}>

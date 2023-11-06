85707519cc8cc71ef5cf1417e9dcda6aa79bdd30
GaoZeWen <1440651163@qq.com>
2023-11-06 21:57:34 +0800

    feat: 表格横向滚动 TODO

--- src/pages/questionnaire/statistic/MainPanel/index.tsx
+++ src/pages/questionnaire/statistic/MainPanel/index.tsx
@@ -1,8 +1,9 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
 import { Spin, Table, Tooltip, Typography } from 'antd'
 import { useParams } from 'react-router-dom'
 import { useRequest } from 'ahooks'
 import { ColumnsType } from 'antd/es/table'
+import { Reference } from 'rc-table'
 import styles from './index.module.scss'
 import { getStatisticListService } from '../../../../services/statistic'
 import { getVisibleComponentList } from '../../../../store/utils'
@@ -22,6 +23,7 @@ const MainPanel: FC<PropsType> = props => {
   const { id = '' } = useParams()
   const [total, setTotal] = useState(0)
   const [list, setList] = useState([])
+  const tableRef = useRef<Reference>(null)
   const { componentList } = useGetQEditorInfo()
 
   const { loading } = useRequest(
@@ -53,6 +55,7 @@ const MainPanel: FC<PropsType> = props => {
     {
       title: '答卷ID',
       dataIndex: '_id',
+      key: '_id',
       fixed: 'left',
       width: 80,
       ellipsis: {
@@ -86,6 +89,7 @@ const MainPanel: FC<PropsType> = props => {
             </div>
           ),
           dataIndex: compInfo.fe_id,
+          key: compInfo.fe_id,
           width: getColumnWidthByCompType(compInfo.type),
           ellipsis: {
             showTitle: false,
@@ -100,15 +104,28 @@ const MainPanel: FC<PropsType> = props => {
   ]
 
   const scrollX = tableColumns.reduce((total, cur) => total + parseInt(String(cur.width || 0)), 0)
+
+  useEffect(() => {
+    if (tableRef.current) {
+      console.log('gzw====>tableRef.current', tableRef.current)
+      console.log('gzw====>nativeElement', tableRef.current.nativeElement)
+      console.log('gzw====>scrollTo', tableRef.current.scrollTo)
+      // tableRef.current.scrollTo({ index: 9 })
+      tableRef.current.nativeElement.scrollLeft = 1300
+    }
+  }, [selectedCompId])
+
   const TableElement = (
     <>
       <Table
+        ref={tableRef}
         columns={tableColumns}
         dataSource={list}
         pagination={false}
         rowKey="_id"
         // 550 是 10 条数据所对应的表格高度（不连表头）
-        scroll={{ x: scrollX, y: 550 }}
+        // scroll={{ x: scrollX, y: 550 }}
+        scroll={{ x: scrollX, y: 250 }}
       />
     </>
   )

19339621bba6cd379cfaf6b2ae73c5e60a080d0c
gaozewen <1440651163@qq.com>
2023-11-08 08:21:03 +0800

    test: 为 QEditorInfo 编写单元测试

---
+++ src/components/QEditorComponents/QEditorInfo/index.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import QEditorInfo from '.'
+
+test('默认属性', () => {
+  // 渲染组件
+  render(<QEditorInfo />)
+  const h = screen.getByText('问卷标题')
+  // 断言，元素在 document 里
+  expect(h).toBeInTheDocument()
+})
+
+test('传入属性', () => {
+  render(<QEditorInfo title="hello" desc="world" />)
+
+  const h = screen.getByText('hello')
+  expect(h).toBeInTheDocument()
+
+  const p = screen.getByText('world')
+  expect(p).toBeInTheDocument()
+})
+
+test('多行文字', () => {
+  render(<QEditorInfo desc={'a\nb\nc'} />)
+
+  const span = screen.getByText('a')
+  expect(span).toBeInTheDocument()
+
+  expect(span).toHaveTextContent('a')
+  // 被换行了
+  expect(span).not.toHaveTextContent('ab')
+})
